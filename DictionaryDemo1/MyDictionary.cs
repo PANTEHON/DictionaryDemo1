0e0167e2d73a5899069f53f640ca0c4ae21db7ff
Batuhan Arslan <aslanbt.arsln@gmail.com>
2023-06-11 16:39:50 +0300

    Add project files.

---
+++ DictionaryDemo1/MyDictionary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<T1, T2>
+    {
+        T1[] _array1;
+        T2[] _array2;
+        T1[] _tempArray1;
+        T2[] _tempArray2;
+        public MyDictionary()
+        {
+            _array1 = new T1[0];
+            _array2 = new T2[0];
+        }
+
+        public void Add(T1 item1, T2 item2)
+        {
+            _tempArray1 = _array1;
+            _tempArray2 = _array2;
+
+            _array1 = new T1[_array1.Length + 1];
+            _array2 = new T2[_array2.Length + 1];
+
+            for (int i = 0; i < _tempArray1.Length; i++)
+            {
+                _array1[i] = _tempArray1[i];
+            }
+            for (int i = 0; i < _tempArray2.Length; i++)
+            {
+                _array2[i] = _tempArray2[i];
+            }
+
+            _array1[_array1.Length - 1] = item1;
+            _array2[_array2.Length - 1] = item2;
+
+        }
+
+        public void T1Id()
+        {
+            foreach (var item1 in _array1)
+            {
+                Console.WriteLine("Kişi id : " + item1);
+            }
+        }
+
+        public void T2Id()
+        {
+            foreach (var item2 in _array2)
+            {
+                Console.WriteLine("Kişi ismi : " + item2);
+            }
+        }
+
+        public int Count
+        {
+            get { return _array1.Length; }
+
+        }
+    }
+}
---
+++ DictionaryDemo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Batuhan");
+            myDictionary.Add(2, "Zeynep");
+            myDictionary.Add(3, "Mert");
+            myDictionary.Add(4, "Semih");
+            myDictionary.Add(5, "Asena");
+
+            myDictionary.T1Id();
+
+            myDictionary.T2Id();
+
+            Console.WriteLine("Kişi sayısı : " + myDictionary.Count);
+
+        }
+    }
+}
